5022bd2e1b45612817a19b78a97aa3b3d34760d0
Raneem AlRashoud <439200695@student.ksu.edu.sa>
2022-05-20 23:26:48 +0300

    Initial commit

---
+++ VideoCapApp.swift
@@ -0,0 +1,16 @@
+//
+//  VideoCapApp.swift
+//  VideoCap
+//
+//
+
+import SwiftUI
+
+@main
+struct VideoCapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            root()
+        }
+    }
+}
